9a268806e16b3154e5a15275c465d678ee104005
window434 <takanoyuki0830@gmail.com>
2026-09-03 01:16:20 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+alert("うんこ");
 const BUTTON_CLICK_EVENT = document.getElementById("test1");
 BUTTON_CLICK_EVENT.addEventListener("click", () => {
                                    alert("ばか🖕🏿🖕🏻🖕🏽🖕");
