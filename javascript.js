df4d3dbfdec88a5a5c5f3f6875c0225e1eb60610
window434 <takanoyuki0830@gmail.com>
2026-09-03 07:54:06 +0900

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
-alert("うんこ");
+alert("危険⚠️");
+const BUTTON_CLICK_EVENT= document.getElementById('test1');
+        BUTTON_CLICK_EVENT.addEventListener('click', () => {
+            alert("ボタンがクリックされました🖕🖕🏻🖕🏽🖕🏿");
+        });
